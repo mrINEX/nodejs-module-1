1233a88f0ebdd4534d33dc35c0cf215d41d19c2e
mrinex <mrinex@mail.ru>
2021-03-28 23:08:59 +0300

    refactor: error handling

--- src/task-2/app.ts
+++ src/task-2/app.ts
@@ -3,10 +3,15 @@ import { ReadStream, WriteStream } from "node:fs";
 
 const csv = require('csvtojson');
 
+function handling(error: Error): void {
+  process.stdout.write(error.message);
+}
+
 const readable: ReadStream = createReadStream(`${__dirname}/csv/nodejs-hw1-ex1.csv`, { highWaterMark: 2 });
-readable.on('error', (err: Error) => process.stdout.write(err.message));
-
 const writable: WriteStream = createWriteStream(`${__dirname}/txt/content.txt`);
-writable.on('error', (err: Error) => process.stdout.write(err.message));
 
-readable.pipe(csv()).pipe(writable);
+readable
+  .on('error', handling)
+  .pipe(csv())
+  .pipe(writable)
+  .on('error', handling);
