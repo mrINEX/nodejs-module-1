c9d2c20289d504040c51f0cce3dc82be57905694
mrinex <mrinex@mail.ru>
2021-03-28 22:53:30 +0300

    refactor: remove comments

--- src/task-2/app.ts
+++ src/task-2/app.ts
@@ -1,10 +1,6 @@
-// 'https://epa.ms/nodejs19-hw1-ex1'
-
-import { Transform } from "node:stream";
 import { createReadStream, createWriteStream } from 'fs';
 import { ReadStream, WriteStream } from "node:fs";
 
-const http = require('https');
 const csv = require('csvtojson');
 
 const readable: ReadStream = createReadStream(`${__dirname}/csv/nodejs-hw1-ex1.csv`, { highWaterMark: 2 });
@@ -14,23 +10,3 @@ const writable: WriteStream = createWriteStream(`${__dirname}/txt/content.txt`);
 writable.on('error', (err: Error) => process.stdout.write(err.message));
 
 readable.pipe(csv()).pipe(writable);
-
-
-// const options = {
-//   host: 'epa.ms',
-//   path: '/nodejs19-hw1-ex1',
-//   method: 'GET',
-// };
-
-// const request = http.request(options, function (res) {
-//   res.setEncoding('utf8');
-//   // console.log('res:::', res.socket._readableState);
-
-//   res.on('data', function (chunk) {
-//     console.log(chunk);
-//   });
-//   res.on('end', function () {
-//     console.log('end');
-//   });
-// });
-// request.end();
